81062c6a242cf7f38dc57de71058c454d227df26
Mateus Mourão <mateusm@take.net>
2021-07-05 21:39:06 -0300

    add login page template

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,84 @@
-import { Form } from 'antd';
+import "./Login.css";
+
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 export const LoginPage = () => {
+  const [finish, setFinish] = useState(false);
 
-    return (
-        <>
-            <h1>Login page</h1>
-            <Form ></Form>
-        </>
-    );
-}
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <>
+      <div className="login-container">
+        <h1>Login page</h1>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Informe o seu email!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Informe sua senha!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Senha"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Mantenha-me logado</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Esqueci a senha
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              loading="true"
+            >
+              Log in
+            </Button>
+          </Form.Item>
+
+          <p>
+            {" "}
+            Não tem uma conta? <a href="">Registrar-se</a>
+          </p>
+        </Form>
+      </div>
+    </>
+  );
+};
