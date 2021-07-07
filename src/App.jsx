3a1e3e3c9b098897fdd39027759d30e9edcb26bf
Matheus Brant <matheus.brant@upflow.me>
2021-07-07 00:44:32 -0300

    Início página de rankings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MainLayout } from "./layout/MainLayout";
 import { LoginPage } from "./pages/Login";
+import RankingPage from "./pages/Ranking";
 import Home from "./pages/Home";
 
 const RouteWithLayout = ({ component: Component, layout: Layout, ...rest }) => (
@@ -20,6 +21,7 @@ export const App = () => {
       <Router>
         <RouteWithLayout layout={MainLayout} path="/home" component={Home} />
         <Route path="/login" component={LoginPage} />
+        <Route path="/ranking" component={RankingPage} />
       </Router>
     </>
   );
