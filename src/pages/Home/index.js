a0960f87180f8f2eb88196e9aa44916412f3785a
Mateus Mourão <mateusm@take.net>
2021-07-06 22:33:31 -0300

    change Home component to function component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,83 +1,88 @@
-import React from 'react';
-import './Home.css';
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 
-import { Card, Row, Col} from 'antd';
+import { Card, Row, Col } from "antd";
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      songs: [],
-      artists: [],
-    };
-  }
-  
-  async getTopSongs() {
-    const response = await fetch("https://api.vagalume.com.br/rank.php?type=mus&period=week&periodVal=201134&scope=all&limit=10");
-    return await response.json();
-  }
-  
-  async getTopArtists() {
-    const response = await fetch("https://api.vagalume.com.br/rank.php?type=art&period=week&periodVal=201134&scope=all&limit=10");
-    return await response.json();
-  }
+function Home() {
+  const [songs, setSongs] = useState([]);
+  const [artists, setArtists] = useState([]);
 
-  async componentDidMount() {
-    const songs = await this.getTopSongs();
-    const artists = await this.getTopArtists();
-    this.setState({
-      isLoaded: true,
-      songs: songs.mus.week.all,
-      artists: artists.art.week.all
-    });
-  }
+  // executado 1 vez ao renderizar o component
+  useEffect(() => {
+    async function fetchApi() {
+      const songs = await getTopSongs();
+      const artists = await getTopArtists();
 
-  renderSongsli(row) {
-    return <li>
-      <b>{row.name}</b>
-      <p>
-        {row.art.name}
-      </p>
-    </li>
-  }
+      await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  renderArtistli(row) {
-    return <li>
-      <b>{row.name}</b>
-    </li>
-  }
-
-  render() {
-    const { isLoaded, songs, artists } = this.state;
-    if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <>
-          <Row className="Ranking">
-            <Col span={16} offset={4}>
-              <Card className="RankingCard" title="Músicas do momento" extra={<a href="#">Mostrar top 100 músicas</a>} >
-                <div className="RankingItems">
-                  <ol>{songs.map(this.renderSongsli)}</ol>
-                </div>
-              </Card>
-            </Col>
-          </Row>
-          <Row className="Ranking">
-            <Col span={16} offset={4}>
-              <Card className="RankingCard" title="Artistas do momento" extra={<a href="#">Mostrar top 100 artistas</a>} >
-                <div className="RankingItems">
-                  <ol>{artists.map(this.renderArtistli)}</ol>
-                </div>
-              </Card>
-            </Col>
-          </Row>
-        </>
-      );
+      setSongs(songs.mus.week.all);
+      setArtists(artists.art.week.all);
     }
-  }
+
+    fetchApi();
+  }, []);
+
+  const getTopSongs = async () => {
+    const response = await fetch(
+      "https://api.vagalume.com.br/rank.php?type=mus&period=week&periodVal=201134&scope=all&limit=10"
+    );
+    return await response.json();
+  };
+
+  const getTopArtists = async () => {
+    const response = await fetch(
+      "https://api.vagalume.com.br/rank.php?type=art&period=week&periodVal=201134&scope=all&limit=10"
+    );
+    return await response.json();
+  };
+
+  const renderSongsli = (row) => {
+    return (
+      <li>
+        <b>{row.name}</b>
+        <p>{row.art.name}</p>
+      </li>
+    );
+  };
+
+  const renderArtistli = (row) => {
+    return (
+      <li>
+        <b>{row.name}</b>
+      </li>
+    );
+  };
+
+  return (
+    <>
+      <Row className="Ranking">
+        <Col span={16} offset={4}>
+          <Card
+            className="RankingCard"
+            title="Músicas do momento"
+            extra={<a href="#">Mostrar top 100 músicas</a>}
+          >
+            <div className="RankingItems">
+              <ol>{songs.map(renderSongsli)}</ol>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="Ranking">
+        <Col span={16} offset={4}>
+          <Card
+            className="RankingCard"
+            title="Artistas do momento"
+            extra={<a href="#">Mostrar top 100 artistas</a>}
+          >
+            <div className="RankingItems">
+              <ol>{artists.map(renderArtistli)}</ol>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
 export default Home;
