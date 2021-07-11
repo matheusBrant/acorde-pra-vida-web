bab1b17e66130215e875112781bba2c69a3319ed
Matheus Brant <matheus.brant@upflow.me>
2021-07-11 01:14:32 -0300

    Desenho dos gráficos dos rankings

--- src/pages/Ranking/index.jsx
+++ src/pages/Ranking/index.jsx
@@ -1,33 +1,62 @@
+import React from 'react'
 import { Card, Row, Col } from "antd";
+import { Bar } from 'react-chartjs-2';
 
-function RankingPage(props){
+function RankingPage(){
     return (
-        <>
-        <Row gutter={[16, 16]}>
-            <Col span={8} offset={4}><Graphs/>
-            
-            </Col>
-            <Col span={8} offset={4}><h1>teste</h1>
-            
-            </Col>
-
-            <Col span={8} offset={4}><h1>teste</h1>
-            
-            </Col>
-            <Col span={8} offset={4}><Graphs/>
-            
-            </Col>
-        </Row>
-        </>
-    );
+        <div>
+            <Row gutter={[16, 16]}>
+            <Col span={1} offset={1} /><Graphs/>
+            <Col span={1} offset={1}/><Graphs/>
+            <Col span={1} offset={1}/><Graphs/>
+            </Row>
+        </div>
+    )
 }
 
-function Graphs(props){
-    const [countView, setcountView] = useState([]);
-
-    return(
-        <h1>testando</h1>
-    );
+const Graphs = () => {
+    return (
+        <div>
+            <Bar 
+                data={{
+                    labels:['Red','Blue','Yellow','Green',
+                    'Purple','Orange'],
+                    datasets:[
+                        {
+                            label: '# of votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor:[
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }
+                    ]                
+                }}
+                height={200}
+                width={300}
+                options={{
+                    maintainAspectRatio: false,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }}
+            />
+        </div>
+    )
 }
-
 export default RankingPage;
